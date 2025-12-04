bab289818a5637b34fd639588ae5989cedacea5e
zapolnoch <zapolnoch@gmail.com>
2025-12-04 14:46:28 +0300

    feat: api

---
+++ src/shared/api/models/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiV1AuthLoginPost200Response';
+export * from './ApiV1AuthLoginPost400Response';
+export * from './ApiV1AuthLoginPost401Response';
+export * from './ApiV1AuthLoginPost403Response';
+export * from './ApiV1AuthLoginPost404Response';
+export * from './ApiV1AuthLoginPost500Response';
+export * from './ApiV1AuthLoginPostRequest';
+export * from './ApiV1AuthLogoutPost200Response';
+export * from './ApiV1AuthMeGet200Response';
+export * from './ApiV1RoundsGet200Response';
+export * from './ApiV1RoundsGet200ResponseDataInner';
+export * from './ApiV1RoundsGet200ResponsePagination';
+export * from './ApiV1RoundsIdGet200Response';
+export * from './ApiV1RoundsIdGet200ResponseMyStats';
+export * from './ApiV1RoundsIdGet200ResponseTopStatsInner';
+export * from './ApiV1RoundsIdGet200ResponseTopStatsInnerUser';
+export * from './ApiV1RoundsIdTapPost200Response';
